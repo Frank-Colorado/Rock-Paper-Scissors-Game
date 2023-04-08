6683b6fb3d56dffda54077d4b77e334cd2cdcecf
Frank-Colorado <chawnkiee@gmail.com>
2023-04-07 20:33:14 -0400

    changed 'hands' ID name to 'content'

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
+const scoreText = document.querySelector("#player-score");
+const contentText = document.querySelector("#content");
+const resultText = document.querySelector("#result");
+
 const playGame = () => {
   const rpsBtn = document.querySelectorAll(".rpsButton");
-  console.log(rpsBtn);
   rpsBtn.forEach((btn) => {
     btn.onclick = () => {
       console.log(btn.value);
+      scoreText.innerText = `This is a test for ${btn.value}`;
     };
   });
 };
