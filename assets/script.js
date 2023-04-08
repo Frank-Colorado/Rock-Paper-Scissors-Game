c497a5d4d238948e2d3c4711a9f6fe6dfe226a16
Frank-Colorado <chawnkiee@gmail.com>
2023-04-07 21:00:08 -0400

    added functions with comments on their processes

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,26 @@ const rpsBtn = document.querySelectorAll(".rpsButton");
 const computerGeneration = () => {
   // This function will generate a random RPS choice from the computer
 };
+const calculateScore = (player, computer) => {
+  //This function will calculate the score off the player choice vs computer choice
+};
 
-//function adds event listener
+const displayDOM = (score, player, computer) => {
+  //This function will change the display of the DOM.
+  //It will display score, player/computer choices, and result of win/loss
+};
+
+const rpsRound = (player) => {
+  // This function will call computerGeneration
+  // Then it will take both player/computer values and send to calculateScore function
+  // Then it will take player/computer/score values and send to displayDOM function
+};
+// This function adds onclick to all rpsBtns and initiates the game
+// Then sends player value to rpsRound function
 const playGame = () => {
   rpsBtn.forEach((btn) => {
     btn.onclick = () => {
-      console.log(btn.value);
-      scoreText.innerText = `This is a test for ${btn.value}`;
+      rpsRound(btn.value);
     };
   });
 };
