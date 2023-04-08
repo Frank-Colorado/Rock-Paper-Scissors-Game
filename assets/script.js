258666890a5a444ecc29294dd6750603561ed8c3
Frank-Colorado <chawnkiee@gmail.com>
2023-04-07 20:38:38 -0400

    added new function called 'computerGeneration'

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,12 @@
-const scoreText = document.querySelector("#player-score");
-const contentText = document.querySelector("#content");
-const resultText = document.querySelector("#result");
+const computerGeneration = () => {
+  // This function will generate a random RPS choice from the computer
+};
 
+//function adds event listener
 const playGame = () => {
-  const rpsBtn = document.querySelectorAll(".rpsButton");
   rpsBtn.forEach((btn) => {
     btn.onclick = () => {
       console.log(btn.value);
-      scoreText.innerText = `This is a test for ${btn.value}`;
     };
   });
 };
