94b52f0c7d465f6e07887d8c6db0975be6c143ca
Frank-Colorado <chawnkiee@gmail.com>
2023-04-07 20:39:55 -0400

    added variables for displaying content on DOM

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+const scoreText = document.querySelector("#player-score");
+const contentText = document.querySelector("#content");
+const resultText = document.querySelector("#result");
+const rpsBtn = document.querySelectorAll(".rpsButton");
+
 const computerGeneration = () => {
   // This function will generate a random RPS choice from the computer
 };
@@ -7,6 +12,7 @@ const playGame = () => {
   rpsBtn.forEach((btn) => {
     btn.onclick = () => {
       console.log(btn.value);
+      scoreText.innerText = `This is a test for ${btn.value}`;
     };
   });
 };
